a0cccb36a5ceba3c74fb606b67f00d80c90755c5
Allan Tan <allanctan@gmail.com>
2023-04-23 09:19:57 +0800

    fix build error

--- src/pages/jurisprudence.jsx
+++ src/pages/jurisprudence.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styles from "@/styles/Jurisprudence.module.css";
+import styles from "@/styles/jurisprudence.module.css";
 import PdfViewer from "@/components/pdf-viewer"
 import JurisprudenceTabs from "@/components/jurisprudence-tab";
 import Paper from '@mui/material/Paper';
